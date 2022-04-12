594c732c0ebc4a698758bcbbc624ed50f43080ec
Atha <74566464+Pandademic@users.noreply.github.com>
2022-04-12 13:26:04 -0400

    change messages + logic
    * logic if the provided directory argument is empty
    * change classifications

--- main.go
+++ main.go
@@ -9,16 +9,19 @@ var(
   port string
 )
 func main() {
-    log.Println("[INIT] started")
+    log.Println("info: starting")
     dir := os.Args[1]
-    log.Println("[INFO] running from static dir:",dir)
+    if(dir == "" || dir == " "){
+     log.Println("fatal: no directory provided")
+     os.Exit(1)
+    }  
+    log.Println("info running from dir:",dir)
     http.Handle("/", http.FileServer(http.Dir(string(dir))))
     if os.Args[2] == "" {
       port = ":5000"
     } else{
       port = ":"+ os.Args[2]
     }
-    log.Println("[INFO]running on port",port)
-
+    log.Println("info: starting on port",port)
     log.Fatal(http.ListenAndServe(port, nil))
 }
