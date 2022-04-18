dd7f084f6dff0e13a1eceadc696639c6e453ab5b
Atha <74566464+Pandademic@users.noreply.github.com>
2022-04-18 09:46:55 -0400

    (feat , bugfix , refactor) check how many args are passed
    * usage options

--- main.go
+++ main.go
@@ -7,20 +7,30 @@ import (
 )
 var(
   port string
+  portNum int
+  dir string
 )
+var usage string = `
+USAGE: webserve DIR PORT
+   DIR is the directory you want to serve
+   PORT is the port to serve it to
+   if no PORT is provided , it will default serve to port 5000
+`
 func main() {
     log.Println("info: starting")
-    dir := os.Args[1]
-    if(len(os.Args) < 1){
-     log.Fatal("fatal: no directory provided\nUSAGE:\nwebserve directory port")
+    portNum = os.Args[2]
+    dir = os.Args[1]
+    if(len(os.Args) < 2){
+     log.Println("fatal: no directory provided")
+     log.Fatal(usage)
     }  
-    log.Println("info running from dir:",dir)
+    log.Println("info: running from dir:",dir)
     http.Handle("/", http.FileServer(http.Dir(string(dir))))
     if os.Args[2] == "" {
       port = ":5000"
     } else{
-      port = ":"+ os.Args[2]
+      port = ":"+ portNum
     }
-    log.Println("info: starting on port ",os.Args[2])
+    log.Println("info: starting on port ",portNum)
     log.Fatal(http.ListenAndServe(port, nil))
 }
