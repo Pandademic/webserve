47c7a7ef9b9fb1433f504867d72b458f1f8aa5f0
Pandademic <atharv.gupte510@gmail.com>
2022-05-14 07:46:33 -0400

    handle ctrl-c and clean up all files

--- main.go
+++ main.go
@@ -1,24 +1,39 @@
 package main
- 
-import (
-    "log"
-    "net/http"
-    "os"
-)
-var(
-  port string
-)
-func main(){
-    log.Println("info: init")
-    dir := os.Args[1]
-    log.Println("info: running from dir:",dir)
-    http.Handle("/", http.FileServer(http.Dir(string(dir))))
-    if os.Args[2] == ""{
-      port = ":5000"
-    }else{
-      port = ":"+ os.Args[2]
-    }
-    log.Println("info: running on port:",port)
 
-    log.Fatal(http.ListenAndServe(port, nil))
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	port string
+)
+
+func handleCtrlC(c chan os.Signal) {
+	sig := <-c
+	log.Println("fatal: recivied ", sig)
+	log.Println("info: inducing graceful shutdown")
+	log.Println("done!")
+	os.Exit(0)
+}
+
+func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go handleCtrlC(c)
+	log.Println("info: init")
+	dir := os.Args[1]
+	log.Println("info: serving from:", dir)
+	http.Handle("/", http.FileServer(http.Dir(string(dir))))
+	if os.Args[2] == "" {
+		port = ":5000"
+	} else {
+		port = ":" + os.Args[2]
+	}
+	log.Println("info: running on port:", port)
+
+	log.Fatal(http.ListenAndServe(port, nil))
 }
